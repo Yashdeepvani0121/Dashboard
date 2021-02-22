cf516c9ef13e3043db5103d23bb09ae209719ff2
Yashdeep vani <62925702+Yashdeepvani0121@users.noreply.github.com>
2021-02-22 14:18:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,652 @@
+var dashboardData = [
+   {
+      "username":"Ram",
+      "orderNo":"0107202001",
+      "totalQty":100,
+      "totalAmount":1000,
+      "city":"Indore",
+      "orderDate":"01-01-2019",
+      "status":" ",
+      "productName":"Coco Cola",
+      "productCode":25,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Coco-Cola",
+      "mrp":10,
+      "outletCode":"001",
+      "outletName":"Coca-Cola",
+      "mobileNo":9876543210,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":10
+   },
+   {
+      "username":"Sita",
+      "orderNo":"0107202002",
+      "totalQty":50,
+      "totalAmount":500,
+      "city":"Pune",
+      "orderDate":"01-05-2020",
+      "status":" ",
+      "productName":"Maaza",
+      "productCode":24,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Coca-Cola",
+      "mrp":10,
+      "outletCode":"002",
+      "outletName":"Coca-Cola",
+      "mobileNo":9765432108,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":50
+   },
+   {
+      "username":"Krishna",
+      "orderNo":"0107202003",
+      "totalQty":45,
+      "totalAmount":450,
+      "city":"Bhopal",
+      "orderDate":"01-02-2020",
+      "status":" ",
+      "productName":"Sprite",
+      "productCode":23,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Coca-Cola",
+      "mrp":10,
+      "outletCode":"003",
+      "outletName":"Coca-Cola",
+      "mobileNo":97654321087,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":45
+   },
+      {
+      "username":"Jayesh",
+      "orderNo":"0107202004",
+      "totalQty":140,
+      "totalAmount":1400,
+      "city":"Alirajpur",
+      "orderDate":"24-08-2020",
+      "status":" ",
+      "productName":"Thums Up",
+      "productCode":22,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Coca-Cola",
+      "mrp":10,
+      "outletCode":"004",
+      "outletName":"Coca-Cola",
+      "mobileNo":9543210876,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":14
+   },
+   {
+      "username":"Dipanshu",
+      "orderNo":"0107202005",
+      "totalQty":75,
+      "totalAmount":750,
+      "city":"Indore",
+      "orderDate":"14-11-2020",
+      "status":" ",
+      "productName":"Limca",
+      "productCode":21,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Coca-Cola",
+      "mrp":10,
+      "outletCode":"005",
+      "outletName":"Coca-Cola",
+      "mobileNo":9432108765,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":7
+   },
+   {
+      "username":"Abhi",
+      "orderNo":"0107202006",
+      "totalQty":200,
+      "totalAmount":2000,
+      "city":"Jabalpur",
+      "orderDate":"02-06-2020",
+      "status":" ",
+      "productName":"Fanta",
+      "productCode":20,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Coca-Cola",
+      "mrp":10,
+      "outletCode":"006",
+      "outletName":"Coca-Cola",
+      "mobileNo":9321087654,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":20
+   },
+   {
+      "username":"Somya",
+      "orderNo":"0107202007",
+      "totalQty":150,
+      "totalAmount":1500,
+      "city":"Guna",
+      "orderDate":"14-06-2020",
+      "status":" ",
+      "productName":"Pepsi",
+      "productCode":19,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"PepsiCo",
+      "mrp":10,
+      "outletCode":"007",
+      "outletName":"PepsiCo",
+      "mobileNo":9210876543,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":15
+   },
+   {
+      "username":"Pushpendra",
+      "orderNo":"0107202008",
+      "totalQty":350,
+      "totalAmount":3500,
+      "city":"Bhopal",
+      "orderDate":"17-08-2020",
+      "status":" ",
+      "productName":"Mirinda",
+      "productCode":18,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"PepsiCo",
+      "mrp":10,
+      "outletCode":"008",
+      "outletName":"PepsiCo",
+      "mobileNo":9108765432,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":35
+   },
+   {
+      "username":"Sumit",
+      "orderNo":"0107202009",
+      "totalQty":90,
+      "totalAmount":900,
+      "city":"Gawlior",
+      "orderDate":"28-07-2020",
+      "status":" ",
+      "productName":"7 Up",
+      "productCode":17,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"PepsiCo",
+      "mrp":10,
+      "outletCode":"009",
+      "outletName":"PepsiCo",
+      "mobileNo":9087654321,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":9
+   },
+   {
+      "username":"Shruti",
+      "orderNo":"0107202010",
+      "totalQty":60,
+      "totalAmount":600,
+      "city":"Nasik",
+      "orderDate":"19-09-2020",
+      "status":" ",
+      "productName":"Mountain Dew",
+      "productCode":16,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"PepsiCo",
+      "mrp":10,
+      "outletCode":"010",
+      "outletName":"PepsiCo",
+      "mobileNo":8765432109,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":6
+   },
+   {
+      "username":"Neha",
+      "orderNo":"0107202011",
+      "totalQty":450,
+      "totalAmount":4500,
+      "city":"Banglore",
+      "orderDate":"01-03-2020",
+      "status":" ",
+      "productName":"Frooti",
+      "productCode":15,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Parle Agro",
+      "mrp":10,
+      "outletCode":"011",
+      "outletName":"Parle Agro",
+      "mobileNo":8654321097,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":45
+   },
+   {
+      "username":"Saloni",
+      "orderNo":"0107202012",
+      "totalQty":300,
+      "totalAmount":3000,
+      "city":"Mhow",
+      "orderDate":"01-01-2019",
+      "status":" ",
+      "productName":"Slice",
+      "productCode":14,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"PepsiCo",
+      "mrp":10,
+      "outletCode":"012",
+      "outletName":"PepsiCo",
+      "mobileNo":8543210976,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":30
+   },
+   {
+      "username":"Shivani",
+      "orderNo":"0107202013",
+      "totalQty":200,
+      "totalAmount":2000,
+      "city":"Mandleshwar",
+      "orderDate":"18-11-2020",
+      "status":" ",
+      "productName":"Minute Maid",
+      "productCode":13,
+      "category":"Juice",
+      "subCategory":"Fruit Juice",
+      "brand":"Coca-Cola",
+      "mrp":10,
+      "outletCode":"013",
+      "outletName":"Coca-Cola",
+      "mobileNo":8432109765,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":20
+   },
+   {
+      "username":"Neeraj",
+      "orderNo":"0107202014",
+      "totalQty":180,
+      "totalAmount":1800,
+      "city":"Dhar",
+      "orderDate":"15-08-2020",
+      "status":" ",
+      "productName":"Glaceau",
+      "productCode":12,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Coca-Cola",
+      "mrp":10,
+      "outletCode":"014",
+      "outletName":"Coca-Cola",
+      "mobileNo":8321097654,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":18
+   },
+   {
+      "username":"Prince",
+      "orderNo":"0107202015",
+      "totalQty":440,
+      "totalAmount":4400,
+      "city":"Hyderabad",
+      "orderDate":"27-12-2020",
+      "status":" ",
+      "productName":"Appletiser",
+      "productCode":11,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Coca-Cola",
+      "mrp":10,
+      "outletCode":"015",
+      "outletName":"Coca-Cola",
+      "mobileNo":8210976543,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":44
+   },
+   {
+      "username":"Yash",
+      "orderNo":"0107202016",
+      "totalQty":440,
+      "totalAmount":4400,
+      "city":"Pune",
+      "orderDate":"21-06-2020",
+      "status":" ",
+      "productName":"Appy Fizz",
+      "productCode":10,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Parle Agro",
+      "mrp":10,
+      "outletCode":"016",
+      "outletName":"Parle Agro",
+      "mobileNo":8109765432,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":44
+   },
+   {
+      "username":"Abhishek",
+      "orderNo":"0107202017",
+      "totalQty":105,
+      "totalAmount":1050,
+      "city":"Delhi",
+      "orderDate":"23-06-2020",
+      "status":" ",
+      "productName":"Red Bull",
+      "productCode":9,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Red Bull",
+      "mrp":10,
+      "outletCode":"017",
+      "outletName":"Red Bull",
+      "mobileNo":8097654321,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":13
+   },
+   {
+      "username":"Nikhil",
+      "orderNo":"0107202018",
+      "totalQty":103,
+      "totalAmount":1030,
+      "city":"Indore",
+      "orderDate":"01-08-2020",
+      "status":" ",
+      "productName":"Appy",
+      "productCode":8,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Parle Agro",
+      "mrp":10,
+      "outletCode":"018",
+      "outletName":"Parle Agro",
+      "mobileNo":8976543210,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":12
+   },
+   {
+      "username":"Jyoti",
+      "orderNo":"0107202019",
+      "totalQty":399,
+      "totalAmount":3990,
+      "city":"Badwani",
+      "orderDate":"01-01-2021",
+      "status":" ",
+      "productName":"Paper Boat",
+      "productCode":7,
+      "category":"Juice",
+      "subCategory":"Fruit Juice",
+      "brand":"Hector Beverages",
+      "mrp":10,
+      "outletCode":"019",
+      "outletName":"Paper Boat",
+      "mobileNo":7654321098,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":39
+   },
+   {
+      "username":"Shyam",
+      "orderNo":"0107202020",
+      "totalQty":173,
+      "totalAmount":1730,
+      "city":"Rau",
+      "orderDate":"14-03-2020",
+      "status":" ",
+      "productName":"Safal",
+      "productCode":6,
+      "category":"Juice",
+      "subCategory":"Fruit Juice",
+      "brand":"Mother Dairy",
+      "mrp":10,
+      "outletCode":"020",
+      "outletName":"Mother Dairy",
+      "mobileNo":7543210986,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":17
+   },
+   {
+      "username":"Vishal",
+      "orderNo":"0107202021",
+      "totalQty":222,
+      "totalAmount":2220,
+      "city":"Sehore",
+      "orderDate":"13-07-2020",
+      "status":" ",
+      "productName":"Jeeru",
+      "productCode":5,
+      "category":"Cold Drink",
+      "subCategory":"Soft Drink",
+      "brand":"Xotik Frujus",
+      "mrp":10,
+      "outletCode":"021",
+      "outletName":"Xotik Frujus",
+      "mobileNo":7432109865,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":22
+   },
+   {
+      "username":"Asha",
+      "orderNo":"0107202022",
+      "totalQty":167,
+      "totalAmount":1670,
+      "city":"Ratlam",
+      "orderDate":"22-02-2020",
+      "status":" ",
+      "productName":"B-Natural",
+      "productCode":4,
+      "category":"Juice",
+      "subCategory":"Fruit Juice",
+      "brand":"ITC",
+      "mrp":10,
+      "outletCode":"022",
+      "outletName":"ITC",
+      "mobileNo":7321098654,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":16
+   },
+   {
+      "username":"Mohit",
+      "orderNo":"0107202023",
+      "totalQty":178,
+      "totalAmount":1780,
+      "city":"Dhar",
+      "orderDate":"01-04-2020",
+      "status":" ",
+      "productName":"Real Fruit",
+      "productCode":3,
+      "category":"Juice",
+      "subCategory":"Fruit Juice",
+      "brand":"Dabur",
+      "mrp":10,
+      "outletCode":"023",
+      "outletName":"Dabur",
+      "mobileNo":7210986543,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":18
+   },
+   {
+      "username":"Grami",
+      "orderNo":"0107202024",
+      "totalQty":60,
+      "totalAmount":600,
+      "city":"Dhar",
+      "orderDate":"07-01-2020",
+      "status":" ",
+      "productName":"Tropicana",
+      "productCode":2,
+      "category":"Juice",
+      "subCategory":"Fruit Juice",
+      "brand":"PepsiCo",
+      "mrp":10,
+      "outletCode":"024",
+      "outletName":"PepsiCo",
+      "mobileNo":7109865432,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":6
+   },
+   {
+      "username":"Sonu",
+      "orderNo":"0107202025",
+      "totalQty":333,
+      "totalAmount":3330,
+      "city":"Indore",
+      "orderDate":"06-01-2020",
+      "status":" ",
+      "productName":"Fruit Juices",
+      "productCode":1,
+      "category":"Juice",
+      "subCategory":"Fruit Juice",
+      "brand":"Patanjali",
+      "mrp":10,
+      "outletCode":"025",
+      "outletName":"Patanjali",
+      "mobileNo":7098654321,
+      "address":"",
+      "supplier":"India",
+      "totalProductCount":33
+   },
+   ]
+
+var topOrders = []
+var bottomOrders = []
+var topUsers = []
+var bottomUsers = []
+var tableHeaders = {
+    topOrder: ["Order Number", "Total Quantity", "Total Amount", "User Name"],
+    topUser: ["User Name", "Total Quantity", "Total Amount", "City"],
+    orderDetail: ["User Name", "Order Number","Order Date", "Status", "Total Amount", "Total Quantity", "Total Product Count"]
+}
+
+function compare(a,b){
+if(a.totalAmount < b.totalAmount){
+    return -1;
+}
+if(a.totalAmount > b.totalAmount){
+    return 1;
+}
+return 0;
+}
+
+function renderTableDOM(tableData, tableId, tableHeaders){
+   var html = "<table class='table'>";
+    html+="<tr>";
+    html+="<th>"+tableHeaders[0]+"</th>"
+    html+="<th>"+tableHeaders[1]+"</th>"
+    html+="<th>"+tableHeaders[2]+"</th>"
+    html+="<th>"+tableHeaders[3]+"</th>"
+    html+="</tr>";
+
+   for(var i =0; i<5; i++){
+    html+="<tr>";
+    html+="<td>"+tableData[i].orderNo+"</td>"
+    html+="<td>"+tableData[i].totalQty+"</td>"
+    html+="<td>"+tableData[i].totalAmount+"</td>"
+    html+="<td>"+tableData[i].username+"</td>"
+    html+="</tr>";
+   }
+   html+= "</table>";
+   document.getElementById(tableId).innerHTML = html;
+}
+
+var topOrders  = JSON.parse(JSON.stringify(dashboardData.sort(compare).reverse()));
+var bottomOrders  = JSON.parse(JSON.stringify(dashboardData.sort(compare)));
+renderTableDOM(topOrders, "topOrderId", tableHeaders.topOrder);
+renderTableDOM(bottomOrders, "bottomOrderId", tableHeaders.topOrder);
+
+//row 4 contain
+
+function renderTablesDOM(tableData, tableId, tableHeaders){
+   var html = "<table class='table'>";
+    html+="<tr>";
+    html+="<th>"+tableHeaders[0]+"</th>"
+    html+="<th>"+tableHeaders[1]+"</th>"
+    html+="<th>"+tableHeaders[2]+"</th>"
+    html+="<th>"+tableHeaders[3]+"</th>"
+    html+="</tr>";
+
+   for(var i =0; i<5; i++){
+    html+="<tr>";
+    html+="<td>"+tableData[i].username+"</td>"
+    html+="<td>"+tableData[i].totalQty+"</td>"
+    html+="<td>"+tableData[i].totalAmount+"</td>"
+    html+="<td>"+tableData[i].city+"</td>"
+    html+="</tr>";
+   }
+   html+= "</table>";
+   document.getElementById(tableId).innerHTML = html;
+}
+
+var topUsers  = JSON.parse(JSON.stringify(dashboardData.sort(compare).reverse()));
+var bottomUsers  = JSON.parse(JSON.stringify(dashboardData.sort(compare)));
+renderTablesDOM(topUsers, "topUserId", tableHeaders.topUser);
+renderTablesDOM(bottomUsers, "bottomUserId", tableHeaders.topUser);
+
+//Chart Data Contain
+var ctx = document.getElementById("myChart").getContext('2d');
+
+var myChart = new Chart(ctx, {
+    type: 'line',
+        data: {
+        labels: ["01-07-2020",   "02-07-2020",   "02-07-2020",  "04-07-2020", "05-07-2020",    "06-07-2020", "07-07-2020","08-07-2020",   "09-07-2020","10-07-2020"],
+        datasets: [{
+            label: 'Order Amount', // Name the series
+            data: [500, 50, 2424,   14040,  14141,  4111,   4544,   47, 5555, 6811], // Specify the data values array
+            fill: false,
+            borderColor: '#2196f3', // Add custom color border (Line)
+            backgroundColor: '#2196f3', // Add custom color background (Points and Fill)
+            borderWidth: 2 // Specify bar border width
+        }]},
+    options: {
+      responsive: true, // Instruct chart js to respond nicely.
+      maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height 
+    }
+});
+
+//row 5 Data Contain
+function renderDataDOM(tableData, tableId, tableHeaders){
+   var html = "<table class='table'>";
+    html+="<tr>";
+    html+="<th>"+tableHeaders[0]+"</th>"
+    html+="<th>"+tableHeaders[1]+"</th>"
+    html+="<th>"+tableHeaders[2]+"</th>"
+    html+="<th>"+tableHeaders[3]+"</th>"
+    html+="<th>"+tableHeaders[4]+"</th>"
+    html+="<th>"+tableHeaders[5]+"</th>"
+    html+="<th>"+tableHeaders[6]+"</th>"
+    html+="</tr>";
+
+   for(var i =0; i<dashboardData.length; i++){
+    html+="<tr>";
+    html+="<td>"+tableData[i].username+"</td>"
+    html+="<td>"+tableData[i].orderNo+"</td>"
+    html+="<td>"+tableData[i].orderDate+"</td>"
+    html+="<td>"+tableData[i].status+"</td>"
+    html+="<td>"+tableData[i].totalAmount+"</td>"
+    html+="<td>"+tableData[i].totalQty+"</td>"
+    html+="<td>"+tableData[i].totalProductCount+"</td>"
+    html+="</tr>";
+   }
+   html+= "</table>";
+   document.getElementById(tableId).innerHTML = html;
+}
+
+renderDataDOM(dashboardData, "orderDetailId", tableHeaders.orderDetail);
